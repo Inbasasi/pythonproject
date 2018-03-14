db30f990160b1b3ad8c18e4b2211c2e0b211afbc
Inbasasi <inbas777@gmail.com>
2018-03-14 07:55:43 +0530

    Add files via upload

--- backend_task.py
+++ backend_task.py
@@ -6,10 +6,10 @@ Created on Mon Mar 12 23:46:50 2018
 """
 
 import urllib.request, json,re
-#with urllib.request.urlopen("https://gist.githubusercontent.com/murtuzakz/4bd887712703ff14c9b0f7c18229b332/raw/d0dd1c59016e2488dcbe0c8e710a1c5df9c3672e/season7.json") as url:
-   #data = json.loads(url.read().decode())
-with open('C:\\Users\\Inba\\Desktop\\file.json') as json_data:
-  data = json.load(json_data)
+with urllib.request.urlopen("https://gist.githubusercontent.com/murtuzakz/4bd887712703ff14c9b0f7c18229b332/raw/d0dd1c59016e2488dcbe0c8e710a1c5df9c3672e/season7.json") as url:
+   data = json.loads(url.read().decode())
+#with open('C:\\Users\\Inba\\Desktop\\file.json') as json_data:
+  #data = json.load(json_data)
 
 main=[]
 epi = ['Episode 1','Episode 2','Episode 3','Episode 4','Episode 5','Episode 6','Episode 7','Episode 8','Episode 9','Episode 10','Episode 11','Episode 12','Episode 13','Episode 14','Episode 15','Episode 16','Episode 17','Episode 18','Episode 19','Episode 20','Episode 21','Episode 22','Episode 23','Episode 24']
